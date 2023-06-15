327ce40dcb01d2de104b65e3085ed00cb49ef9a5
mahdi388 <ahangarym34@yahoo.com>
2023-06-15 13:48:31 +0330

    add navbar

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import './styles/main.scss'
+import welcomeImage from './images/welcome.jpg'
 
 class App extends Component {
-  render() { 
+  render() {
     return (
       <>
-        <h1>hello</h1>
+        <nav className='nav-bar'>
+          <ul>
+            <li>
+              <div></div>
+              Home
+              <div></div>
+            </li>
+            <li>
+              <div></div>
+              Movies
+              <div></div>
+            </li>
+            <li>
+              <div></div>
+              About us
+              <div></div>
+            </li>
+          </ul>
+          <img src={welcomeImage} alt="welcome" />
+        </nav>
       </>
     );
   }
 }
- 
+
 export default App;
