4ca6d79b2778a7624a466cdeee9971c2846c0285
mahdi388 <ahangarym34@yahoo.com>
2023-06-21 12:57:33 +0330

    add about us page

--- movies-app/src/components/AboutUs.js
+++ movies-app/src/components/AboutUs.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import '../styles/about-us.scss'
+import img from '../images/my-image.jpg'
 
 class AboutUs extends Component {
     render() { 
-        return (<>About us</>);
+        return (
+            <div className="container">
+                <img src={img} alt="Mohammad Mahdi Ahangari" />
+                <div>
+                    Hello. I am Mohammad Mahdi Ahangari. I am a web developer from Hamedan. I'm 13 years old. My birthday is on March 13, 2010.
+                    I like programming, watching movies, watching football and gaming. I am a student from elmi school.
+                    My favorite football team is Real Madrid and my favorite football player is Ronaldo. I can programming with Python, Django, HTML, CSS, JavaScript and React.
+                </div>
+            </div>
+        );
     }
 }
- 
+
 export default AboutUs;
